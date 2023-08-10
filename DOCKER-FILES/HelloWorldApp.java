bf5e0de79ee4211fe111eb891c1498fdc24f69ee
SONAL SINGH <117289524+isonalam@users.noreply.github.com>
2023-08-11 00:00:35 +0530

    Update and rename java application to HelloWorldApp.java

--- DOCKER-FILES/HelloWorldApp.java
+++ DOCKER-FILES/HelloWorldApp.java
@@ -2,6 +2,6 @@ FROM openjdk:11
 MAINTAINER sonalpriya1597@gmail.com
 WORKDIR data
 COPY  . . 
-RUN javac Hello.java
+RUN javac HelloWorldApp.java
 EXPOSE 8080
-CMD ["java","Hello"]
+CMD ["java","HelloWorldApp"]
